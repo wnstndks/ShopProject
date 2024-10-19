cb589f00662f3b827fcc2744d64f96c77943ed66
junsu <anjs9912@gmail.com>
2024-10-20 00:30:29 +0900

    이차 수정

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const image = ["/img/fs1.jpg", "/img/fs2.jpg", "/img/fs3.jpg", "/img/fs4.jpg"];
+  const image = ["/img/fs1.jpg", "/img/fs2.jpg", "/img/fs3.jpg"];
 
   const imageSets = [
     { left: "/img/arsenal.jpg", right: "/img/totenham.jpg" },
@@ -24,6 +24,7 @@ export default function Home() {
     const offset = scrollY - index * viewportHeight;
     const progress = Math.max(0, Math.min(offset / viewportHeight, 1));
 
+    // Move the image up by 20% of its height
     const translateY = progress * 20;
 
     return {
@@ -34,16 +35,14 @@ export default function Home() {
 
   return (
     <div className="relative">
-      {/* 이미지들을 고정된 크기로 표시 */}
+      {/* image 배열을 반복해서 각각 렌더링 */}
       {image.map((imgSrc, index) => (
-        <div key={index} className="relative h-[80vh] w-full overflow-hidden"> {/* 높이 명시 */}
+        <div key={index} className="relative h-screen w-full sticky top-0 overflow-hidden">
           <Image
-            src={imgSrc}
-            alt={`Image ${index + 1}`}
-            layout="responsive" /* 레이아웃을 고정된 비율로 */
-            width={1920} /* 이미지를 적당한 해상도로 표시 */
-            height={1080}
-            objectFit="contain" /* 이미지를 잘리지 않게 */
+            src={imgSrc} // 템플릿 리터럴을 사용하여 이미지 경로 설정
+            alt={`${index + 1}`}
+            fill
+            style={{ objectFit: "cover" }}
           />
         </div>
       ))}
@@ -60,20 +59,16 @@ export default function Home() {
                 <Image
                   src={set.left}
                   alt={`Left ${index + 1}`}
-                  layout="responsive"
-                  width={960}
-                  height={1080}
-                  objectFit="contain"
+                  fill
+                  style={{ objectFit: "cover" }}
                 />
               </div>
               <div className="flex-1 relative">
                 <Image
                   src={set.right}
                   alt={`Right ${index + 1}`}
-                  layout="responsive"
-                  width={960}
-                  height={1080}
-                  objectFit="contain"
+                  fill
+                  style={{ objectFit: "cover" }}
                 />
               </div>
             </div>
